2402f8f7d71c17a09a2e81e525219c442d60790d
Chris Ghiorzi <seethedata@gmail.com>
2016-07-09 13:55:33 -0400

    Simplified IE translation

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ var httpServer = http.Server(app);
 
 
 
-function getName() {
-	return "Philadelphia, PA";
-}
-
-app.set('views',__dirname + "/views");
-app.set('view engine', 'jade');
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res){
 	res.render('index.html');
 });
+
+
+
 app.listen(process.env.port || 8080);
